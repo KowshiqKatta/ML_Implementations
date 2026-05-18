5ad91c73c7363c82269c295c2ec60b59dfc45949
KowshiqKatta <kattamurikowshiq@gmail.com>
2026-05-18 21:27:31 +0530

    implemented normalization

--- feature_engineering/feature_scaling.py
+++ feature_engineering/feature_scaling.py
@@ -1,3 +1,5 @@
+# standardization
+
 import numpy as np
 import pandas as pd
 
@@ -24,6 +26,23 @@ standardized_df = pd.DataFrame(standardized_data, columns=data_df.columns)
 print(standardized_data)
 print(standardized_df.head())
 
+# normalization
 
+data = np.random.uniform(low = 10, high = 100, size = (1000, 2))
+data_df = pd.DataFrame(data, columns=['Feature1', 'Feature2'])
 
+print(data_df.head())
 
+plt.hist(data_df['Feature1'])
+plt.hist(data_df['Feature2'])
+
+plt.show()
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+scaled_data = scaler.fit_transform(data_df) 
+scaled_df = pd.DataFrame(scaled_data, columns=data_df.columns)
+
+print(scaled_data)
+print(scaled_df.head())
