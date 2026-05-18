74e808a924d019a6e6ca1181636e9e2f2d1912e3
KowshiqKatta <kattamurikowshiq@gmail.com>
2026-05-18 21:53:00 +0530

    implemented IQR

--- feature_engineering/outliers_removal.py
+++ feature_engineering/outliers_removal.py
@@ -20,3 +20,29 @@ threshold = 3
 
 df['outlier'] = np.abs(df['z_score']) > threshold
 print(df)
+
+df_clean = df[df['outlier'] == False]
+print(df_clean)
+
+# IQR method
+
+data = {
+    'Value': np.append(np.random.normal(50, 10, 100), [150, 200, 250, 300, 350, 400])
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+Q1 = df['Value'].quantile(0.25)
+Q3 = df['Value'].quantile(0.75)
+
+IQR = Q3 - Q1
+
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+df['outlier'] = (df['Value'] < lower_bound) | (df['Value'] > upper_bound)
+print(df)
+
+df_clean = df[df['outlier'] == False]
+print(df_clean)
