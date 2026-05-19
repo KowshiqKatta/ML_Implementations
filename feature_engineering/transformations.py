3459ed00a1d63f4269f884c1a1cf9f28c93e9944
KowshiqKatta <kattamurikowshiq@gmail.com>
2026-05-19 17:25:31 +0530

    log transformation added

---
+++ feature_engineering/transformations.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import skew
+
+# log transformation
+
+np.random.seed(0)
+
+data = {
+    'value': np.random.exponential(scale=2.0, size=1000)
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+
+print(f"Skewness of 'value' column: {skew(df['value'])}")
+
+df['log_transformed_value'] = np.log(df['value'] + 1) # Adding 1 to avoid log(0)
+
+print(f"Skewness of 'log_transformed_value' column: {skew(df['log_transformed_value'])}")
