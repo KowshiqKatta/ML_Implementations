e628bfb46cd78ea0cfa3cf04260bfe2885abffe4
KowshiqKatta <kattamurikowshiq@gmail.com>
2026-05-21 19:42:32 +0530

    implemented methods to handle time data

--- feature_engineering/complex_handling.py
+++ feature_engineering/complex_handling.py
@@ -18,6 +18,20 @@ df['day_of_week'] = df['date'].dt.dayofweek # extra feature
 
 print(df.head())
 
+# time series 
 
+num_periods = 100
+date_rng = pd.date_range(start='2023-01-01', periods=num_periods, freq='h')
 
+df = pd.DataFrame(date_rng, columns=['timestamp'])
+print(df.head())
+
+df['year'] = df['timestamp'].dt.year
+df['month'] = df['timestamp'].dt.month
+df['day'] = df['timestamp'].dt.day
+df['hour'] = df['timestamp'].dt.hour
+df['minute'] = df['timestamp'].dt.minute
+df['second'] = df['timestamp'].dt.second
+
+print(df.head())
 
