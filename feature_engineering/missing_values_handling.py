0253fc17762140148aceaefc2da24babcae00ce1
KowshiqKatta <kattamurikowshiq@gmail.com>
2026-05-22 13:32:17 +0530

    implemented mean imputation

---
+++ feature_engineering/missing_values_handling.py
@@ -0,0 +1,17 @@
+# mean imputation
+
+import numpy as np
+import pandas as pd
+from sklearn.impute import SimpleImputer
+
+data = {
+    'Value': [1, 2, np.nan, 4, 5, np.nan, 7]
+}
+
+df = pd.DataFrame(data)
+print(df.head())
+
+imputer = SimpleImputer(strategy='mean')
+df['transformed_value'] = imputer.fit_transform(df[['Value']])
+
+print(df)
